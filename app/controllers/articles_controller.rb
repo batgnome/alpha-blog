7203d46ad9428887a2ddf4be1332260b57e43bbd
batgnome <fouflord@gmail.com>
2021-01-27 12:03:05 -0700

    stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,8 @@
 class ArticlesController < ApplicationController
-
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def show
 
-		@article = Article.find(params[:id])
 	end
 
 	def index
@@ -14,14 +13,44 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+
+	end
+
 	def create
-		@article	= Article.new(params.require(:article).permit(:title, :description))
+		@article	= Article.new(article_params)
 		if @article.save
-			flash[:notice] = "article was created successfully."
+			flash[:notice] = "Article was created successfully."
 			redirect_to  @article
 		else
 			render 'new'
 		end
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+			flash[:notice] = " Article was updated successfully"
+		else
+			render 'edit'
+		end			
+
+	end
+
+	def destroy
+
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
+	def set_article
+		@article = Article.find(params[:id])
 	end
 
 end
